1ee6a9bf389e1c243d1f416cd2e851c1e0419b6d
amiosamu <damitullaev@gmail.com>
2025-10-08 20:37:07 +0200

    fix: make content wider

--- app/essays/[slug]/page.tsx
+++ app/essays/[slug]/page.tsx
@@ -45,7 +45,7 @@ export default function EssayPage({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      <div className="container mx-auto px-4 py-16 max-w-2xl">
+      <div className="container mx-auto px-4 py-16 max-w-4xl">
         <Link 
           href="/essays/"
           className="inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-foreground transition-colors mb-8 group"
